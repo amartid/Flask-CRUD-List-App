c887dfe3dbade3d3e06ca7ee6f7fd7c47c537a85
amartid <64313984+amartid@users.noreply.github.com>
2023-12-17 23:37:46 +0200

    last commit

--- app.py
+++ app.py
@@ -1,45 +1,51 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
-from datetime import date
-import json
-import requests
-import pandas as pd
-import os
-import shutil
-import seaborn as sns
-import matplotlib.pyplot as plt
-import warnings
-from openpyxl import Workbook
-from collections import OrderedDict
-from pathlib import Path
-
-
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persons.db'
 db = SQLAlchemy(app)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"{self.id} - {self.name} - {self.age}"
+        return f'<Person {self.name}>'
+
+@app.route('/add_person', methods=['GET', 'POST'])
+def add_person():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_person = Person(name=name)
+        db.session.add(new_person)
+        db.session.commit()
+        return redirect(url_for('list_persons'))
+    return render_template('add_person.html')
 
 @app.route('/')
-def index():
-    persons = Person.query.order_by(Person.id).all()
-    return render_template('persons.html', persons=persons)
+@app.route('/list_persons')
+def list_persons():
+    persons = Person.query.all()
+    return render_template('index.html', persons=persons)
 
-@app.route('/dashboard')
-def dashboard():
-    # Add logic to generate data for the dashboard
-    data = {'key': 'value'}
-    return render_template('dashboard.html', data=data)
+@app.route('/edit_person/<int:id>', methods=['GET', 'POST'])
+def edit_person(id):
+    person = Person.query.get(id)
+    if request.method == 'POST':
+        person.name = request.form['name']
+        db.session.commit()
+        return redirect(url_for('list_persons'))
+    return render_template('edit_person.html', person=person)
+
+@app.route('/delete_person/<int:id>')
+def delete_person(id):
+    person = Person.query.get(id)
+    db.session.delete(person)
+    db.session.commit()
+    return redirect(url_for('list_persons'))
 
 if __name__ == '__main__':
-    # Create all database tables
     with app.app_context():
         db.create_all()
     app.run(debug=True)
